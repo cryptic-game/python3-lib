562e3aa0835291600768c56ab45854ce3b758f05
mieasy <mieasy@pm.me>
2019-04-15 19:18:25 +0200

    added config and mysql support

--- example.py
+++ example.py
@@ -1,6 +1,10 @@
-from cryptic import MicroService
+from cryptic import MicroService, setup_database, get_config, Config
 
+# important: set the mode before initializing anything else
+config: Config = get_config(mode="debug")
+# config.set_mode("debug")  # this is the other possibility to set the mode
 ms: MicroService = MicroService(name="echo")
+engine, Base, session = setup_database()
 
 
 @ms.microservice_endpoint(path=["microservice"])
